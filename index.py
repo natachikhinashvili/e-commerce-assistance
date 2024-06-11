3411a4534eccbd7c58f92f510318a8b942a830e2
natachikhinashvili <natanatuka33@gmail.com>
2024-06-11 19:00:26 +0400

    feat: app

--- index.py
+++ index.py
@@ -1,19 +1,26 @@
-from flask import Flask, request, jsonify
-import spacy
+from flask import Flask, request, jsonify, render_template
 from search import search_products
 from processdata import tfidf_matrix, vectorizer, df
 
 app = Flask(__name__)
 
-nlp = spacy.load("en_core_web_sm")
-user = input('How can i help you?')
-
-result = nlp(user)
 
 @app.route('/', methods=['GET'])
-def search():
-    results = search_products(user, tfidf_matrix, vectorizer, df)
-    return jsonify(results.to_dict(orient='records'))
+def main():
+    return render_template('index.html')
+
+
+@app.route('/get', methods=['GET', 'POST'])
+def search():    
+    user_input = request.args.get('msg')
+    app.logger.info(f"Received user input: {user_input}")
+    result = search_products(user_input, tfidf_matrix, vectorizer, df)
+
+    response_text = f"Top result for you :\n{result['Product Name']} - {result['Selling Price']} - {result['Product Url']}"
+
+    app.logger.info(f"Response: {response_text}")
+    return jsonify(response=response_text)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- search.py
+++ search.py
@@ -1,19 +1,11 @@
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
-from processdata import tfidf_matrix, vectorizer, df
 
 def search_products(query, tfidf_matrix, vectorizer, df):
     query_vec = vectorizer.transform([query])
-
     cosine_similarities = cosine_similarity(query_vec, tfidf_matrix).flatten()
 
-    top_indices = np.argsort(cosine_similarities)[-10:][::-1]
+    top_index = np.argmax(cosine_similarities)
 
-    results = df.iloc[top_indices]
-    return results
-
-user_query = "red dress"
-
-results = search_products(user_query, tfidf_matrix, vectorizer, df)
-
-print(results)
+    result = df.iloc[top_index]
+    return result
